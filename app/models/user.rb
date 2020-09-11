54a61852b74823e89bc0e2356caebdec8f3cc307
Combatd <combatdstar@gmail.com>
2020-09-10 17:11:22 -0700

    Create User model validations

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+
+class User < ApplicationRecord
+    validates :password_digest, presence: true
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    attr_reader :password # password getter method
+
+    # after_initialize :ensure_session_token
