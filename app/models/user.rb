52ae0d9af19640815ced2a56ce0031e8e52a9086
Combatd <combatdstar@gmail.com>
2020-09-11 17:14:08 -0700

    MEncapsulate Users session_token data

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,14 @@ class User < ApplicationRecord
 
     # after_initialize :ensure_session_token
 
-    def self.generate_session_token
-        SecureRandom::urlsafe_base64(16)
-    end
-
     def reset_session_token!
         self.session_token = User.generate_session_token
         self.save!
         self.session_token # return the session token
+    end
+
+    private
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
     end
