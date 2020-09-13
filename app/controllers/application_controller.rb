1b4d5950de3cdeb62d33a4e3fef5b9c801844c97
Combatd <combatdstar@gmail.com>
2020-09-13 11:21:43 -0700

    Define ApplicationController#log_in_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    private
+
+    def log_in_user!(user)
+        user.reset_session_token!
+        session[:session_token] = user.session_token
+    end
 end
