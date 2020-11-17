d6d74e8b5489e8f7ad9710f7f4a94edebb9e10e0
Combatd <combatdstar@gmail.com>
2020-11-17 08:44:32 -0800

    Define BandCsController#create action

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -5,6 +5,17 @@ class BandsController < ApplicationController
         render :index
     end
 
+    def create
+        @band = Band.new(band_params)
+
+        if @band.save
+            redirect_to band_url(@band)
+        else
+            flash.now[:errors] = @band.errors.full_messages
+            render :new
+        end
+    end
+
     private # hidden encapsulated data
 
     def band_params
