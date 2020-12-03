1c23561ce366bf69753d5ff83feb1e517894be74
Combatd <combatdstar@gmail.com>
2020-12-03 05:23:59 -0800

    Define AlbumsController::album_params to accept form data properties

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,10 @@
 class AlbumsController < ApplicationController
+    # albums is nested in bands - no index action
+
+
+    private # hidden encapsulated data
+
+   def album_params
+    params.require(:album).permit(:title, :year, :band_id, :live!)
+   end 
 end
