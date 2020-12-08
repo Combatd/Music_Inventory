b63395cbdce3f1557be79e8a9a6847f1031fd34d
Combatd <combatdstar@gmail.com>
2020-12-08 07:53:13 -0800

    Add model validations to Track Model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,5 @@
 class Track < ApplicationRecord
+    validates :title, :ord, presence: true # :lyrics is optional
+    validates :bonus, inclusion: { in: [true, false] }
+    validates :ord, uniqueness: { scope: :album_id }
 end
