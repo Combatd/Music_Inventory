b92559b70f03e1be80452cd98e1005d3a2fa6a9d
Combatd <combatdstar@gmail.com>
2020-12-08 08:01:00 -0800

    Define TracksController#new action

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,13 @@
 class TracksController < ApplicationController
     before_action :require_log_in!
 
+    def new
+        @album = Album.find(params[:album_id])
+        @band = Band.find(params[@album.band_id]
+        @track = Track.new(album_id: params[:album_id]))
+        render :new
+    end
+
     private # hidden encapsulated data
 
     def track_params
