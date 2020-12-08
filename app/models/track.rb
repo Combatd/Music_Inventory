300f58a19589d92e62e8e4e91c5a33389165a244
Combatd <combatdstar@gmail.com>
2020-12-08 07:54:59 -0800

    Define Track Model associations

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,10 @@ class Track < ApplicationRecord
     validates :title, :ord, presence: true # :lyrics is optional
     validates :bonus, inclusion: { in: [true, false] }
     validates :ord, uniqueness: { scope: :album_id }
+
+    belongs_to :album
+
+    has_one :band,
+        through: :album,
+        source: :band
 end
