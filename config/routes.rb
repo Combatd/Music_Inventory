5570b4ff120f163d7d39344b9a4757507f9ecd8d
Combatd <combatdstar@gmail.com>
2020-12-08 09:02:27 -0800

    Debug typo in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :albums, only: [:new]
   end
   resources :albums, except: [:new, :index] do
-    resources :tracks, only [:new]
+    resources :tracks, only: [:new]
   end
   resources :tracks, except: [:index, :new]
 end
