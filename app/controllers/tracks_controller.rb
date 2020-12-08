a068d357994d4f1f479ae1167526208c93b38e62
Combatd <combatdstar@gmail.com>
2020-12-08 09:06:38 -0800

    Define TracksController#create action

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -8,6 +8,17 @@ class TracksController < ApplicationController
         render :new
     end
 
+    def create
+        @track = Track.new(track_params)
+
+        if @track.save!
+            redirect_to track_url(@track)
+        else
+            flash.now[:errors] = @track.errors.full_messages
+            render :new
+        end
+    end
+
     private # hidden encapsulated data
 
     def track_params
