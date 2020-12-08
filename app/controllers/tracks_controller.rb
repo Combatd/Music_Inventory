16fcaba70e02181d41b73e58d7c5a28c9e7a5e36
Combatd <combatdstar@gmail.com>
2020-12-08 07:59:08 -0800

    Define TracksController##track_params

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,4 +1,9 @@
 class TracksController < ApplicationController
     before_action :require_log_in!
 
+    private # hidden encapsulated data
+
+    def track_params
+        params.require(:track).permit(:title, :ord, :lyrics, :album_id, :bonus)
+    end
 end
